b3b2ead67b3a464ff1f16ac71fcb09335cd15685
fadime <fadimekomurcu@hotmail.com>
2022-03-28 09:37:07 -0400

    trycloudAutomation-Group6 proje created

---
+++ src/test/java/com/TryCloud/runners/CukesRunner.java
@@ -0,0 +1,24 @@
+package com.TryCloud.runners;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "html:target/cucumber-report.html",
+                "rerun:target/rerun.txt",
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber"
+
+        },
+        features = "src/test/resources/features",
+        glue = "com/TryCloud/step_definitions",
+        dryRun = false,
+        tags = ""
+)
+public class CukesRunner {
+
+
+}
